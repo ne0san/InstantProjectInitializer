6ce36603c83f118bf239683b4b5f7c5497ca4f65
ne0san <ne0l1gh7@vivaldi.net>
2026-07-31 21:45:44 +0900

    init

---
+++ devenv.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # F# has no dedicated devenv language option.
+  packages = [
+    pkgs.fsharp
+  ];
+  # https://devenv.sh/languages/
+  languages = {
+    dotnet.enable = true;
+    dotnet.package = pkgs.dotnet-sdk;
+  };
+  # See full reference at https://devenv.sh/reference/options/
+}
+
---
+++ tests/InstantProjectInitializer.Tests/Main.fs
@@ -0,0 +1,6 @@
+﻿module InstantProjectInitializer.Tests
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssemblyWithCLIArgs [] argv
