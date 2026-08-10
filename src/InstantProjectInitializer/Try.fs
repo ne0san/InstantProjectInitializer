9ce0bcdf1bd3adafc53a3ca92811d45c32b50adc
ne0san <ne0l1gh7@vivaldi.net>
2026-08-10 18:09:14 +0900

    Try 変換モジュール作成

---
+++ src/InstantProjectInitializer/Try.fs
@@ -0,0 +1,16 @@
+namespace InstantProjecoInitializer
+
+module Try =
+    let ofCatch (f: 'a -> 'b) (a: 'a) =
+        try f a |> Ok
+        with ex -> Error [ ex.Message ]
+
+    let ofAsyncCatch (f: 'a -> Async<'b>) (a: 'a) =
+        async {
+            try
+                let! x = f a
+                return Ok x
+            with
+            | ex -> return Error [ ex.Message ]
+        }
+
