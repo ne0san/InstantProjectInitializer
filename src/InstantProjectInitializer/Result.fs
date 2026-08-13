13873b537371ce7e291fa3ee576eb1a00ca78480
ne0san <ne0l1gh7@vivaldi.net>
2026-08-13 18:37:00 +0900

    Tryモジュール名変更->Result

--- src/InstantProjectInitializer/Result.fs
+++ src/InstantProjectInitializer/Result.fs
@@ -1,5 +1,10 @@
 [<RequireQualifiedAccess>]
-module Try
+// TODO: ResultもしくはValidationモジュールの拡張に変える
+// TODO: 指定のエラーだけ変換できるようにする
+
+// NOTE: System.AggregateException内部のSystem.Net.Http.HttpRequestException
+// NOTE: そもそもfshttpnに置き換えたら変わるのでは
+module Result
 
 let ofCatch (f: 'a -> 'b) (a: 'a) =
     try f a |> Ok
