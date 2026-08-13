ad51c2bde0e7e4d88b326dde5c5a7eaaad255ccd
ne0san <ne0l1gh7@vivaldi.net>
2026-08-14 00:20:17 +0900

    各種バージョン調整

--- devenv.nix
+++ devenv.nix
@@ -10,7 +10,7 @@
   # https://devenv.sh/languages/
   languages = {
     dotnet.enable = true;
-    dotnet.package = pkgs.dotnet-sdk;
+    dotnet.package = pkgs.dotnet-sdk_10;
   };
   # See full reference at https://devenv.sh/reference/options/
 }
